76f560378b7853fa69fec0d3751305d935e8987b
yossy11 <srnnds50aky1@yahoo.co.jp>
2021-01-06 22:57:49 +0900

    fix bugs

--- src/Yld2004SwiftNonAFRCBE_numerical.for
+++ src/Yld2004SwiftNonAFRCBE_numerical.for
@@ -25,8 +25,8 @@
       DOUBLE PRECISION YOUNG,POISSON,HARDK,HARDN,HARDSTRAIN0,lame,
      & shearMod,eStrain(6),pStrain(6),eqpStrain,totalStrain(6),
      & yldCPrime(6,6),yldCDbPrime(6,6),hillParams(4),eqStress,
-     & flowStress,calc_eqStress,calc_FlowStress,dfdS(6),dGdS(6),
-     & eqGStress,calc_eqGStress,dLambda,F,H,lambda
+     & calc_eqStress,eqGStress,calc_eqGStress,lambda,trialStress(6),
+     & trialeqpStrain,dGdS(6)
 
       ! define constants
       PARAMETER(YOUNG=6.9D4,POISSON=0.33D0,HARDK=646.0D0,HARDN=0.227D0,
@@ -121,11 +121,11 @@
       INCLUDE 'ABA_PARAM.INC'
       INTEGER YLDM
       DOUBLE PRECISION DDSDDE(6,6),yldCPrime(6,6),yldCDbPrime(6,6),
-      STRESS(6),hillParams(4),HARDK,HARDN,HARDSTRAIN0,eqpStrain,lambda,
-      TOLER,invDDSDDE(6,6),eqStress,calc_eqStress,eqGStress,
-      calc_eqGStress,flowStress,calc_FlowStress,initialF,F,dfdS(6),
-      dGdS(6),ddGddS(6,6),H,a0(6),b0,A(7,7),invA(7,7),vec1(7),vec2(7),
-      vec3(7),numerator,denominator,dLambda,increment(7)
+     & STRESS(6),hillParams(4),HARDK,HARDN,HARDSTRAIN0,eqpStrain,lambda,
+     & TOLER,invDDSDDE(6,6),eqStress,calc_eqStress,eqGStress,
+     & calc_eqGStress,flowStress,calc_FlowStress,initialF,F,dfdS(6),
+     & dGdS(6),ddGddS(6,6),H,a0(6),b0,A(7,7),invA(7,7),vec1(7),vec2(7),
+     & vec3(7),numerator,denominator,dLambda,increment(7)
       PARAMETER(TOLER=1.0D-1)
       CALL calc_Inverse(6,DDSDDE,invDDSDDE)
       eqStress = calc_eqStress(YLDM,yldCPrime,yldCDbPrime,STRESS)
