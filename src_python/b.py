d2ed187154e6cddfbeb6cb5bfa4a55432286745c
yossy11 <srnnds50aky1@yahoo.co.jp>
2021-01-19 15:53:07 +0900

    update gradient

--- src_python/b.py
+++ src_python/b.py
@@ -11,24 +11,29 @@ yld_T = np.array([[2, -1, -1, 0, 0, 0],
                   [0, 0, 0, 0, 3, 0],
                   [0, 0, 0, 0, 0, 3]])
 yld_T = yld_T/3.0
-c_prime = np.array([-0.0698, 0.9364, 0.0791, 1.0030, 0.5247, 1.3631, 0.9543, 1.0690, 1.0237])
-c_Db_prime = np.array([0.9811, 0.4767, 0.5753, 0.8668, 1.1450, -0.0792, 1.4046, 1.1471, 1.0516])
+c_params = np.array([-0.0698, 0.9364, 0.0791, 1.0030, 0.5247, 1.3631, 0.9543, 1.0690, 1.0237,
+                     0.9811, 0.4767, 0.5753, 0.8668, 1.1450, -0.0792, 1.4046, 1.1471, 1.0516])
 YLDM = 8
 
 
 def make_C_matrix(c_params):
-    C = np.array([[0, -1.0*c_params[0], -1.0*c_params[1], 0, 0, 0],
-                  [-1.0*c_params[2], 0, -1.0*c_params[3], 0, 0, 0],
-                  [-1.0*c_params[4], -1.0*c_params[5], 0, 0, 0, 0],
-                  [0, 0, 0, c_params[6], 0, 0],
-                  [0, 0, 0, 0, c_params[7], 0],
-                  [0, 0, 0, 0, 0, c_params[8]]])
-    return C
+    yld_C_prime = np.array([[0, -1.0*c_params[0], -1.0*c_params[1], 0, 0, 0],
+                            [-1.0*c_params[2], 0, -1.0*c_params[3], 0, 0, 0],
+                            [-1.0*c_params[4], -1.0*c_params[5], 0, 0, 0, 0],
+                            [0, 0, 0, c_params[6], 0, 0],
+                            [0, 0, 0, 0, c_params[7], 0],
+                            [0, 0, 0, 0, 0, c_params[8]]])
+    yld_C_Db_prime = np.array([[0, -1.0*c_params[9], -1.0*c_params[10], 0, 0, 0],
+                               [-1.0*c_params[11], 0, -1.0*c_params[12], 0, 0, 0],
+                               [-1.0*c_params[13], -1.0*c_params[14], 0, 0, 0, 0],
+                               [0, 0, 0, c_params[15], 0, 0],
+                               [0, 0, 0, 0, c_params[16], 0],
+                               [0, 0, 0, 0, 0, c_params[17]]])
+    return yld_C_prime, yld_C_Db_prime
 
 
-def calc_phi(stress, c_prime, c_Db_prime, YLDM):
-    yld_C_prime = make_C_matrix(c_prime)
-    yld_C_Db_prime = make_C_matrix(c_Db_prime)
+def calc_phi(stress, c_params, YLDM):
+    yld_C_prime, yld_C_Db_prime = make_C_matrix(c_params)
     s_prime = yld_C_prime@yld_T@stress
     s_Db_prime = yld_C_Db_prime@yld_T@stress
     s_prime_mat = np.array([[s_prime[0], s_prime[3], s_prime[4]],
@@ -47,32 +52,59 @@ def calc_phi(stress, c_prime, c_Db_prime, YLDM):
     return phi
 
 
-def calc_angled_eqStress(angle, c_prime, c_Db_prime, YLDM):
-    angled_stress = np.array([math.cos(angle)**2, math.sin(angle)**2,
-                              0, math.sin(angle)*math.cos(angle), 0, 0])
-    phi = calc_phi(angled_stress, c_prime, c_Db_prime, YLDM)
+def calc_angled_eqStress(angle, c_params, YLDM):
+    if angle == "EB":
+        angled_stress = np.array([0, 0, 1, 0, 0, 0])
+    else:
+        angle = math.pi*float(angle)/180.0
+        angled_stress = np.array([math.cos(angle)**2, math.sin(angle)**2,
+                                  0, math.sin(angle)*math.cos(angle), 0, 0])
+    phi = calc_phi(angled_stress, c_params, YLDM)
     angled_eqStress = (4/phi)**(1/YLDM)
     return angled_eqStress
 
 
-def error_func(exp_data, c_prime, c_Db_prime, YLDM, wp, wb):
+def error_func(exp_data, c_params, YLDM, wp, wb):
     error = 0
     for data in exp_data:
-        if not data["orientation"].isdecimal():
-            stress = np.array([0, 0, 1, 0, 0, 0])
-            phi = calc_phi(stress, c_prime, c_Db_prime, YLDM)
-            predicted_stress = (4/phi)**(1/YLDM)
-            error += wb*(predicted_stress/float(data["normalized_yield_stress"]) - 1.0)**2
-            continue
-        predicted_stress = calc_angled_eqStress(
-            math.pi*float(data["orientation"])/180.0, c_prime, c_Db_prime, YLDM)
+        predicted_stress = calc_angled_eqStress(data["orientation"], c_params, YLDM)
         print(predicted_stress)
-        error += wp*(predicted_stress/float(data["normalized_yield_stress"])-1.0)**2
+        if data["orientation"] == "EB":
+            error += wb*(predicted_stress/float(data["normalized_yield_stress"])-1.0)**2
+        else:
+            error += wp*(predicted_stress/float(data["normalized_yield_stress"])-1.0)**2
     return error
+
+
+def calc_dsdc(angle, c_params, YLDM):
+    DELTAX = 1.0e-6
+    dsdc = np.ones(18)
+    predicted_stress = calc_angled_eqStress(angle, c_params, YLDM)
+    for i in range(18):
+        sub_c_params = c_params.copy()
+        sub_c_params[i] += DELTAX
+        sub_predicted_stress = calc_angled_eqStress(angle, sub_c_params, YLDM)
+        dsdc[i] = (sub_predicted_stress - predicted_stress)/DELTAX
+    return dsdc
+
+
+def calc_error_gradient(exp_data, c_params, YLDM, wp, wb):
+    gradient = np.zeros(18)
+    for data in exp_data:
+        predicted_stress = calc_angled_eqStress(data["orientation"], c_params, YLDM)
+        dsdc = calc_dsdc(data["orientation"], c_params, YLDM)
+        if data["orientation"] == "EB":
+            gradient += (wb*2.0*(predicted_stress/data["normalized_yield_stress"]-1.0) /
+                         data["normalized_yield_stress"])*dsdc
+        else:
+            gradient += (wp*2.0*(predicted_stress / data["normalized_yield_stress"]-1.0) /
+                         data["normalized_yield_stress"])*dsdc
+    return gradient
 
 
 if __name__ == "__main__":
     with open("Datas/AA2090-T3.csv") as f:
         reader = csv.DictReader(f)
         exp_data = [row for row in reader]
-    print(error_func(exp_data, c_prime, c_Db_prime, YLDM, 1.0, 0.01))
+    print(error_func(exp_data, c_params, YLDM, 1.0, 0.01))
+    print(calc_dsdc(math.pi/3, c_params, YLDM))
