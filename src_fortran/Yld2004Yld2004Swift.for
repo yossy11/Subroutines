a998560f60f3948347fb4cf2244d56be84234442
yossy11 <srnnds50aky1@yahoo.co.jp>
2021-02-24 11:43:08 +0900

    fix error

--- src_fortran/Yld2004Yld2004Swift.for
+++ src_fortran/Yld2004Yld2004Swift.for
@@ -26,7 +26,7 @@
      & dgds(6),H,calc_H,dLambda,F,lambda
 
       DOUBLE PRECISION HARDK,HARDN,HARDSTRAIN0,YLDM,f_yldCPrime(6,6),
-      f_yldCDbPrime(6,6),g_yldCPrime(6,6),g_yldCDbPrime(6,6)
+     & f_yldCDbPrime(6,6),g_yldCPrime(6,6),g_yldCDbPrime(6,6)
 
       ! define constants
       PARAMETER(TOLER=1.0D-5,YOUNG=6.9D4,POISSON=0.33D0)
@@ -38,49 +38,49 @@
       ! anisotropic params
       f_yldCPrime(:,:) = 0.0D0
       f_yldCDbPrime(:,:) = 0.0D0
-                       
-      f_yldCPrime(1,2) = -0.5677369567466022D0
-      f_yldCPrime(1,3) = -1.3048918894938901D0
-      f_yldCPrime(2,1) = -0.6041274337462236D0
-      f_yldCPrime(2,3) = -1.3619107041480343D0
-      f_yldCPrime(3,1) = -1.403576218080736D0
-      f_yldCPrime(3,2) = -1.2047066930584855D0
-      f_yldCPrime(4,4) = 0.5352287733257995D0
-      f_yldCPrime(5,5) = 1.0D0
-      f_yldCPrime(6,6) = 1.0D0
 
-      f_yldCDbPrime(1,2) = -0.5480817415053474D0
-      f_yldCDbPrime(1,3) = -1.2470032531974167D0
-      f_yldCDbPrime(2,1) = -0.3903214745928533D0
-      f_yldCDbPrime(2,3) = -1.3585844128262983D0
-      f_yldCDbPrime(3,1) = -1.2696881944669043D0
-      f_yldCDbPrime(3,2) = -1.3944105663130655D0
-      f_yldCDbPrime(4,4) = 0.6643249523965057D0
-      f_yldCDbPrime(5,5) = 1.0D0
-      f_yldCDbPrime(6,6) = 1.0D0
+      f_yldCPrime(1,2) = -0.056940277665336686D0
+      f_yldCPrime(1,3) = -0.9859971641591349D0
+      f_yldCPrime(2,1) = -0.1986277232332533D0
+      f_yldCPrime(2,3) = -0.9313609018430112D0
+      f_yldCPrime(3,1) = -0.5871124291038275D0
+      f_yldCPrime(3,2) = -1.330942716974344D0
+      f_yldCPrime(4,4) = 0.944649833438939D0
+      f_yldCPrime(5,5) = 1.090740680806561D0
+      f_yldCPrime(6,6) = 1.0925155792832137D0
+
+      f_yldCDbPrime(1,2) = -1.0763323731674885D0
+      f_yldCDbPrime(1,3) = -0.4323653254398786D0
+      f_yldCDbPrime(2,1) = -0.613688607514406D0
+      f_yldCDbPrime(2,3) = -0.9659074930180515D0
+      f_yldCDbPrime(3,1) = -1.1519059774170437D0
+      f_yldCDbPrime(3,2) = 0.08362991282554624D0
+      f_yldCDbPrime(4,4) = 1.4567012369610057D0
+      f_yldCDbPrime(5,5) = 1.1752482654026506D0
+      f_yldCDbPrime(6,6) = 1.116415832572442D0
 
       g_yldCPrime(:,:) = 0.0D0
       g_yldCDbPrime(:,:) = 0.0D0
 
-      g_yldCPrime(1,2) = -0.9900726733680804D0
-      g_yldCPrime(1,3) = -1.0148351190489264D0
-      g_yldCPrime(2,1) = -0.9726232443854402D0
-      g_yldCPrime(2,3) = -1.039810462417897D0
-      g_yldCPrime(3,1) = -1.0322865627637845D0
-      g_yldCPrime(3,2) = -1.0223590160295464D0
-      g_yldCPrime(4,4) = 0.9923216698310934D0
-      g_yldCPrime(5,5) = 1.0D0
-      g_yldCPrime(6,6) = 1.0D0
+      g_yldCPrime(1,2) = 0.0681890550605269D0
+      g_yldCPrime(1,3) = -0.8728581783781829D0
+      g_yldCPrime(2,1) = -0.07883817293470376D0
+      g_yldCPrime(2,3) = -0.9800868482532625D0
+      g_yldCPrime(3,1) = -0.6396161030250869D0
+      g_yldCPrime(3,2) = -1.416739220885185D0
+      g_yldCPrime(4,4) = 1.0767566538602826D0
+      g_yldCPrime(5,5) = 1.069D0
+      g_yldCPrime(6,6) = 1.0237D0
 
-      g_yldCDbPrime(1,2) = -0.9900734717541109D0
-      g_yldCDbPrime(1,3) = -1.01483733432813D0
-      g_yldCDbPrime(2,1) = -0.9726225048559088D0
-      g_yldCDbPrime(2,3) = -1.03981519087893D0
-      g_yldCDbPrime(3,1) = -1.0322854648096973D0
-      g_yldCDbPrime(3,2) = -1.0223590263214553D0
-      g_yldCDbPrime(4,4) = 0.9923238700196437D0
-      g_yldCDbPrime(5,5) = 1.0D0
-      g_yldCDbPrime(6,6) = 1.0D0
+      g_yldCDbPrime(1,2) = -0.936165230037105D0
+      g_yldCDbPrime(1,3) = -0.7827698684119997D0
+      g_yldCDbPrime(2,1) = -0.6837450696952991D0
+      g_yldCDbPrime(2,3) = -0.7782197960468684D0
+      g_yldCDbPrime(3,1) = -1.1210834461178005D0
+      g_yldCDbPrime(3,2) = 0.0930311520854942D0
+      g_yldCDbPrime(4,4) = 1.1682692085440296D0
+      g_yldCDbPrime(5,5) = 1.1471D0
+      g_yldCDbPrime(6,6) = 1.0516D0
 
       ! this subroutine can be used only on the condition of NDI=NSHR=3
       IF (NDI/=3 .or. NSHR/=3) THEN
@@ -223,7 +223,7 @@
      & invDDSDDE(6,6),A(7,7),B(6,6),invB(6,6),C(7,7),lambda,h0,
      & subVec(7),vec1(7),vec2(7),STRESS(6),eqpStrain
         
-      DOUBLE PRECISION YLDM,f_yldCDbPrime(6,6),f_yldCDbPrime(6,6),
+      DOUBLE PRECISION YLDM,f_yldCPrime(6,6),f_yldCDbPrime(6,6),
      & g_yldCPrime(6,6),g_yldCDbPrime(6,6),HARDK,HARDSTRAIN0,HARDN
 
       eqStress = calc_eqStress(YLDM,f_yldCPrime,f_yldCDbPrime,STRESS)
@@ -312,7 +312,7 @@
       SUBROUTINE calc_dfds(YLDM,f_yldCPrime,f_yldCDbPrime,STRESS,dfds)
       IMPLICIT NONE
       DOUBLE PRECISION dfds(6)
-      DOUBLE PRECISION YLDM,f_yldCDbPrime(6,6),f_yldCDbPrime(6,6),
+      DOUBLE PRECISION YLDM,f_yldCPrime(6,6),f_yldCDbPrime(6,6),
      & STRESS(6)
       CALL calc_yld2004_1st_differential(YLDM,f_yldCPrime,f_yldCDbPrime,
      & STRESS,dfds)
@@ -324,7 +324,7 @@
       SUBROUTINE calc_dgds(YLDM,g_yldCPrime,g_yldCDbPrime,STRESS,dgds)
       IMPLICIT NONE
       DOUBLE PRECISION dgds(6)
-      DOUBLE PRECISION YLDM,g_yldCDbPrime(6,6),g_yldCDbPrime(6,6),
+      DOUBLE PRECISION YLDM,g_yldCPrime(6,6),g_yldCDbPrime(6,6),
      & STRESS(6)
       CALL calc_yld2004_1st_differential(YLDM,g_yldCPrime,g_yldCDbPrime,
      & STRESS,dgds)
@@ -338,7 +338,7 @@
       IMPLICIT NONE
       DOUBLE PRECISION ddgdds(6,6)
       DOUBLE PRECISION YLDM,g_yldCPrime(6,6),g_yldCDbPrime(6,6),
-     & STRESS(6),dgds(6),ddgdds(6,6)
+     & STRESS(6),dgds(6)
       CALL calc_yld2004_2nd_differential(YLDM,g_yldCPrime,g_yldCDbPrime,
      & STRESS,dgds,ddgdds)
       RETURN
@@ -448,7 +448,7 @@
         subStress(:) = STRESS(:)
         subStress(i) = subStress(i) + DELTAX
         CALL calc_yld2004_1st_differential(YLDM,yldCPrime,yldCDbPrime,
-     &   STRESS,subfirstDiff)
+     &   subStress,subfirstDiff)
         secondDiff(:,i) = (subfirstDiff(:) - firstDiff(:))/DELTAX
       END DO  
       RETURN
